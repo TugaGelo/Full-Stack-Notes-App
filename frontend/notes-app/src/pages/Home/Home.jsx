b3dcc32d20aa1d710a0444da8495e20ac193dfb1
Tugs <rolandtugaoen013@gmail.com>
2024-07-03 19:12:42 +0800

    Finalized UI

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -46,10 +46,19 @@ const Home = () => {
           },
         }}  
         contentLabel=""
-        className=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-auto"
       >
 
-      <AddEditNotes />
+        <AddEditNotes 
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ 
+              isShown: false,
+              type: "add",
+              data: null});
+          }}  
+        />
       </Modal> 
 
     </>
